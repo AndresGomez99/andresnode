336ef128a858ed8dc5868bbcacca557549ff94f7
AndresGomez99 <48743247+AndresGomez99@users.noreply.github.com>
2021-03-01 11:42:46 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello mundo y personas!')
+  response.send('Hello mundo y personas garay!')
 })
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
